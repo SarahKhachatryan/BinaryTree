7e16e0378ed7a1c96dd37b53a1bdce2a9b4964a4
SarahKhachatryan <71638201+SarahKhachatryan@users.noreply.github.com>
2020-12-13 15:55:07 +0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+BinaryTree bt = new BinaryTree();
+            bt.add(6);
+            bt.add(4);
+            bt.add(8);
+            bt.add(3);
+            bt.add(5);
+            bt.add(7);
+            bt.add(9);
+        System.out.println(bt.containsNode(12));
+        bt.delete(12);
+        System.out.println(bt.containsNode(12));
+
+    }
+}
